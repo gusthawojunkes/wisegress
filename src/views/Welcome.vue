360dcb6612f5cc2e1af91cbce1617e6f6605ad38
Nathan Stüber Antunes <51455214+Nathan-Stuber-Antunes@users.noreply.github.com>
2023-09-18 19:48:01 -0300

    Feat/add landing page (#2)
    * feat: :heavy_plus_sign: add vuetify
    
    * feat(page): :sparkles: add landing page
    
    add landing page with links to other pages
    
    * refactor: :fire: removing vue default files
    
    removed pages, components and icons that are generated by default by Vue and will not be used
    
    * fix(page): :bug: adjust on routerlink

---
+++ src/views/Welcome.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="h-screen">
+        <v-sheet class="d-flex flex-wrap h-screen">
+            <v-sheet class="d-flex flex-1-0 align-end pa-14">
+                <div>
+                    <p class="text-h5">Bem Vindo ao</p>
+                    <p class="text-h1">Wisegress</p>
+                </div>
+            </v-sheet>
+
+            <v-sheet class="d-flex flex-0-1 pa-14">
+                <RouterLink to="/cadastro">
+                    <v-btn id="register" class="ma-2">Cadastre-se</v-btn>
+                </RouterLink>
+                <RouterLink to="/login">
+                    <v-btn id="login" class="ma-2">Acessar</v-btn>
+                </RouterLink>
+            </v-sheet>
+        </v-sheet>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'Welcome',
+    data() {
+        return {
+        }
+    }
+}
+</script>
+  
+<style scoped>
+#login {
+    background-color: #47667B;
+}
+
+#register {
+    background-color: #D5E0B5;
+}
+</style>
