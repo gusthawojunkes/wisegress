77af57da0651b53d0b4a7e6ea5ca4213440057de
Nathan <nathan.stuber.a@gmail.com>
2023-09-25 22:09:51 -0300

    feat(page): :sparkles: added profile registration page
    added the user profile registration page, but it is still under development, just putting some example cards on screen

---
+++ src/views/ProfileRegister.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <Questions :variants="variants"></Questions>
+    </div>
+</template>
+
+<script>
+import Questions from '../components/Question.vue'
+
+export default {
+    name: 'ProfileRegister',
+    data() {
+        return {
+            variants: [
+                'primary',
+                'secondary',
+                'accent',
+                'error',
+                'warning',
+                'info',
+                'success'
+            ],
+            questions: [{
+                "question": "Quais são seus planos para usar o Wisegress?",
+                "answers": [{
+                    "icon": "book",
+                    "title": "Estudo"
+                },
+                {
+                    "icon": "laptop",
+                    "title": "Trabalho"
+                }
+                ]
+            }]
+        }
+    },
+    methods: {
+    },
+    components: {
+        Questions
+    }
+}
+</script>
+
+<style scoped></style>
