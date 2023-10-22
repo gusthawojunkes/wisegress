be2fff78f3ea524c39dab2ae8ebcf9953ae58364
Nathan <nathan.stuber.a@gmail.com>
2023-10-22 14:52:47 -0300

    feat(page): :sparkles: profile registration completed
    Completed the Question component and the ProfileRegister page.

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,14 +1,16 @@
 <template>
-    <v-container fluid>
-        <h3>{{ question.name }}</h3>
+    <v-container>
+        <v-row class="d-flex justify-center">
+            <h3 class="mb-12">{{ question.name }}</h3>
+        </v-row>
         <v-row class="d-flex justify-center">
             <v-col v-for="(answer, i) in question.answers" :key="i" cols="auto">
-                <v-card class="mx-auto">
+                <v-card class="pa-12" style="border: 1px solid #A5C3A7;" @click="cardClick(question.answers[i].title)">
                     <v-card-item>
-                        <svg-icon type="mdi" :path="path" class="svg-icon"></svg-icon>
-                        <v-card-subtitle>
+                        <svg-icon type="mdi" :path="md[question.answers[i].icon]" size="100" color="#47667B"></svg-icon>
+                        <v-card-title class="text-center">
                             {{ answer.title }}
-                        </v-card-subtitle>
+                        </v-card-title>
                     </v-card-item>
                 </v-card>
             </v-col>
@@ -18,22 +20,30 @@
 
 <script>
 import SvgIcon from '@jamescoyle/vue-icon';
-import * as mdi from '@mdi/js';
-
+import * as md from '@mdi/js';
 export default {
     name: 'Question',
     props: {
         question: {
             type: Object,
             required: true
+        },
+        answers: {
+            type: Array,
+            required: true
         }
     },
     components: {
         SvgIcon
     },
-    data() {
-        return {
-            path: mdi.mdiBookOpenBlankVariant,
+    computed: {
+        md() {
+            return md;
+        },
+    },
+    methods: {
+        cardClick(answer) {
+            this.$emit('selectedAnswer', answer);
         }
     }
 }
--- src/views/ProfileRegister.vue
+++ src/views/ProfileRegister.vue
@@ -1,11 +1,28 @@
 <template>
-    <v-container class="h-screen" h-100 fluid>
-        <!-- <v-row class="text-center">
-            <v-col cols="12"> -->
-        <Question :question="questions[0]"></Question>
-        <h6>{{ "Sua colaboração é fundamental para um começo bem-sucedido com o Wisegress." }}</h6>
-        <!-- </v-col>
-        </v-row> -->
+    <v-container class="h-screen d-flex align-center" h-100 fluid>
+        <v-col>
+            <v-row class="d-flex justify-center">
+                <div v-for="(question, q) in questions" :key="q">
+                    <Question v-if="validateShowQuestion(questions, question, q)" :question="question"
+                        @selectedAnswer="addAnswer">
+                    </Question>
+                </div>
+            </v-row>
+            <v-row class="d-flex justify-center" v-if="!showFinishedMessage">
+                <div class="w-25">
+                    <h5 class="mt-12 text-center">
+                        {{ "Sua colaboração é fundamental para um começo bem-sucedido com o Wisegress." }}
+                    </h5>
+                </div>
+            </v-row>
+            <v-row class="d-flex justify-center text-center" v-if="showFinishedMessage">
+                <v-col>
+                    <h5>Obrigado pela contribuição.</h5>
+                    <br>
+                    <h1>Seja Bem Vindo!</h1>
+                </v-col>
+            </v-row>
+        </v-col>
     </v-container>
 </template>
 
@@ -16,6 +33,8 @@ export default {
     name: 'ProfileRegister',
     data() {
         return {
+            showFinishedMessage: false,
+            currentQuantity: null,
             questions: [{
                 "name": "Quais são seus planos para usar o Wisegress?",
                 "answers": [{
@@ -23,18 +42,79 @@ export default {
                     "title": "Estudo"
                 },
                 {
-                    "icon": "laptop",
+                    "icon": "mdiLaptop",
                     "title": "Trabalho"
                 }
-                ]
+                ],
+                "answer": null
+            }, {
+                "name": "Qual é o seu nível de conhecimento em inglês?",
+                "answers": [{
+                    "icon": "mdiBookOpenBlankVariant",
+                    "title": "Básico"
+                },
+                {
+                    "icon": "mdiLaptop",
+                    "title": "Intermediário"
+                },
+                {
+                    "icon": "mdiLaptop",
+                    "title": "Avançado"
+                }
+                ],
+                "answer": null
+            }, {
+                "name": "Qual é o seu nível de conhecimento em programação?",
+                "answers": [{
+                    "icon": "mdiBookOpenBlankVariant",
+                    "title": "Básico"
+                },
+                {
+                    "icon": "mdiLaptop",
+                    "title": "Intermediário"
+                },
+                {
+                    "icon": "mdiLaptop",
+                    "title": "Avançado"
+                }
+                ],
+                "answer": null
             }]
         }
     },
-    methods: {
-    },
     components: {
         Question
-    }
+    },
+    methods: {
+        addAnswer(answer) {
+            this.questions[this.currentQuantity].answer = answer;
+        },
+        validateShowQuestion(questions, question, selectedQuestion) {
+            if ((this.currentQuantity + 1 == questions.length) && (questions[questions.length - 1].answer != null)) {
+                this.showFinishedMessage = true;
+            }
+            if ((question.answer == null) && (selectedQuestion == 0) ||
+                ((selectedQuestion > 0) &&
+                    (questions[selectedQuestion].answer == null) &&
+                    (questions[selectedQuestion - 1].answer != null))) {
+                this.currentQuantity = selectedQuestion;
+                return true
+            }
+            return false;
+        },
+        async onFinished() {
+            setTimeout(() => {
+                this.$router.push('/todo')
+            }, 1000)
+        }
+    },
+    watch: {
+        async showFinishedMessage(newValue) {
+            if (newValue === true) {
+                await this.onFinished()
+            }
+        },
+    },
 }
 </script>
 
