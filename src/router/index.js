3d37ed12a53e6a40918a73277bd83549305637fe
Nathan <nathan.stuber.a@gmail.com>
2023-10-29 15:19:46 -0300

    feat(page): :sparkles: pomodoro development

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Register from '../views/Register.vue'
 import Login from '../views/Login.vue'
 import ProfileRegister from '../views/ProfileRegister.vue'
 import TodoList from '../views/TodoList.vue'
+import Pomodoro from '../views/Pomodoro.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: '/lista-tarefas',
       name: 'TodoList',
       component: TodoList
+    },
+    {
+      path: '/pomodoro',
+      name: 'Pomodoro',
+      component: Pomodoro
     }
   ]
 })
---
+++ src/views/Pomodoro.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-container class="h-screen d-flex align-center w-50">
+        <v-col cols="6" class="text-center">
+            <v-row class="d-flex align-center justify-center mb-6">
+                <h1 class="text-h1">{{ minutes }}:{{ seconds }}</h1>
+            </v-row>
+            <v-row class="d-flex align-center justify-center">
+                <v-col>Foco</v-col>
+                <v-col>
+                    <v-switch class="col-switch" :model-value="isBreak" hide-details color="#F8F4C4"></v-switch>
+                </v-col>
+                <v-col>Pausa</v-col>
+            </v-row>
+            <v-row class=" d-flex align-center justify-center mt-6">
+                <v-btn class="w-50 btn-positive-action" size="large" :text="timeStopped ? `Começar` : `Parar`"
+                    @click="switchTime()"></v-btn>
+            </v-row>
+        </v-col>
+        <v-col cols="6">
+        </v-col>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Pomodoro',
+    data() {
+        return {
+            time: 1500,
+            timeFocus: 1500, // Tempo em segundos (25 minutos)
+            shortBreakTime: 300, // Tempo em segundos (5 minutos)
+            longBreakTime: 900, // Tempo em segundos (15 minutos)
+            timeStopped: true,
+            isBreak: false,
+            numberBreakTime: 0,
+            numberShortStops: 2,
+            numberForLongStop: 3,
+        };
+    },
+    methods: {
+        updateTime() {
+            if (this.timeStopped) return this.time;
+
+            if (this.time > 0) {
+                return this.time--;
+            } else {
+                this.numberBreakTime++;
+
+                if (this.numberBreakTime <= this.numberShortStops) {
+                    this.time = this.shortBreakTime;
+                    this.switchTime()
+                    this.isBreak = true;
+
+                } else if (this.numberBreakTime == this.numberForLongStop) {
+                    this.time = this.longBreakTime;
+                    this.switchTime()
+                    this.isBreak = true;
+
+                } else if (this.numberBreakTime > this.numberForLongStop) {
+                    this.time = this.timeFocus
+                    this.numberBreakTime = 0;
+                    this.switchTime()
+                    this.isBreak = false;
+                }
+            }
+        },
+        switchTime() {
+            this.timeStopped = !this.timeStopped;
+        },
+    },
+    computed: {
+        minutes() {
+            return Math.floor(this.time / 60);
+        },
+        seconds() {
+            return this.time % 60;
+        },
+    },
+    mounted() {
+        setInterval(this.updateTime, 1000);
+    },
+};
+</script>
+
+<style scoped>
+.col-switch {
+    display: flex;
+    justify-content: center;
+}
+</style>
