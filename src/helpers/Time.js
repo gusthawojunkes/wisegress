1241b788c1818fdb2f5aba2830a975e2605f29c1
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2023-11-12 16:28:37 -0300

    wip: pomodoro

--- src/helpers/Time.js
+++ src/helpers/Time.js
@@ -3,4 +3,28 @@ export default class Time {
     static minutesToSeconds(minutes) {
         return (minutes || 0) * 60;
     }
+
+    static saveUnmountTimestamp() {
+        localStorage.setItem("TIME_UNMOUNTED", new Date().getTime().toString());
+    }
+
+    static getUnmountTimestamp() {
+        return Number(localStorage.getItem("TIME_UNMOUNTED"));
+    }
+
+    static savePomodoroGlobalTime(time) {
+        localStorage.setItem("POMODORO", time);
+    }
+
+    static getPomodoroGlobalTime() {
+        return Number(localStorage.getItem("POMODORO"));
+    }
+
+    static getPomodoroActualTime() {
+        const unmountTime = Time.getUnmountTimestamp();
+        const now = new Date().getTime();
+        const diff = Math.ceil(Math.abs(unmountTime - now) / 1000);
+        console.log(diff)
+        return (Time.getPomodoroGlobalTime() - diff).toFixed(0);
+    }
 }
--- src/views/Pomodoro.vue
+++ src/views/Pomodoro.vue
@@ -1,119 +1,125 @@
 <template>
-    <v-container >
-        <v-col class="text-center">
-            <v-row class="d-flex align-center justify-center mb-6">
-                <h1 class="text-h1">{{ minutes }}:{{ seconds }}</h1>
-            </v-row>
-            <v-row class="d-flex align-center justify-center">
-                <v-col>Foco</v-col>
-                <v-col>
-                    <v-switch class="col-switch" :model-value="isBreak" hide-details color="#F8F4C4"></v-switch>
-                </v-col>
-                <v-col>Pausa</v-col>
-            </v-row>
-            <v-row class=" d-flex align-center justify-center mt-6">
-                <v-btn
-                    class="w-50 btn-positive-action"
-                    size="large"
-                    :text="timeStopped ? `Começar` : `Parar`"
-                    @click="switchTime()"
-                ></v-btn>
-                <v-spacer></v-spacer>
-                <v-btn icon="mdi-refresh" @click="restart()"></v-btn>
-            </v-row>
+  <v-container>
+    <v-col class="text-center">
+      <v-row class="d-flex align-center justify-center mb-6">
+        <h1 class="text-h1">{{ minutes }}:{{ seconds < 10 ? '0' + seconds : seconds }}</h1>
+      </v-row>
+      <v-row class="d-flex align-center justify-center">
+        <v-col>Foco</v-col>
+        <v-col>
+          <v-switch class="col-switch" :model-value="isBreak" hide-details color="#F8F4C4"></v-switch>
         </v-col>
-    </v-container>
+        <v-col>Pausa</v-col>
+      </v-row>
+      <v-row class=" d-flex align-center justify-center mt-6">
+        <v-btn
+            class="w-50 btn-positive-action"
+            size="large"
+            :text="timeStopped ? `Começar` : `Parar`"
+            @click="switchTime()"
+        ></v-btn>
+        <v-spacer></v-spacer>
+        <v-btn icon="mdi-refresh" @click="restart()"></v-btn>
+      </v-row>
+    </v-col>
+  </v-container>
 </template>
 
 <script>
-import List from '@/components/List.vue';
 import UserService from "@/services/user.service";
 import Time from "@/helpers/Time";
-import TodoService from "@/services/todo.service";
+
 export default {
-    name: 'Pomodoro',
-    data() {
-        return {
-            time: 1500,
-            timeFocus: 1500,
-            shortBreakTime: 300,
-            longBreakTime: 900,
-            timeStopped: true,
-            isBreak: false,
-            numberBreakTime: 0,
-            numberShortStops: 2,
-            numberForLongStop: 3,
-            previewMode: 'task',
-            tasks: []
-        };
-    },
-    methods: {
-        updateTime() {
-            if (this.timeStopped) return this.time;
+  name: 'Pomodoro',
+  data() {
+    return {
+      time: 1500,
+      timeFocus: 1500,
+      shortBreakTime: 300,
+      longBreakTime: 900,
+      timeStopped: true,
+      isBreak: false,
+      numberBreakTime: 0,
+      numberShortStops: 2,
+      numberForLongStop: 3,
+    };
+  },
+  methods: {
+    updateTime() {
+      if (this.timeStopped) {
+        Time.savePomodoroGlobalTime(this.time);
+        return this.time;
+      }
 
-            if (this.time > 0) {
-                return this.time--;
-            } else {
-                this.numberBreakTime++;
+      if (this.time > 0) {
+        this.time--;
+        Time.savePomodoroGlobalTime(this.time);
+        return this.time;
+      } else {
+        this.numberBreakTime++;
 
-                if (this.numberBreakTime <= this.numberShortStops) {
-                    this.time = this.shortBreakTime;
-                    this.switchTime()
-                    this.isBreak = true;
+        if (this.numberBreakTime <= this.numberShortStops) {
+          this.time = this.shortBreakTime;
+          Time.savePomodoroGlobalTime(this.time);
+          this.switchTime()
+          this.isBreak = true;
 
-                } else if (this.numberBreakTime == this.numberForLongStop) {
-                    this.time = this.longBreakTime;
-                    this.switchTime()
-                    this.isBreak = true;
+        } else if (this.numberBreakTime === this.numberForLongStop) {
+          this.time = this.longBreakTime;
+          Time.savePomodoroGlobalTime(this.time);
+          this.switchTime()
+          this.isBreak = true;
 
-                } else if (this.numberBreakTime > this.numberForLongStop) {
-                    this.time = this.timeFocus
-                    this.numberBreakTime = 0;
-                    this.switchTime()
-                    this.isBreak = false;
-                }
-            }
-        },
-        switchTime() {
-            this.timeStopped = !this.timeStopped;
-        },
-        async getTasks() {
-            return TodoService.findAll();
-        },
-        updateList() {
-            this.tasks = this.getTasks();
-        },
-        restart() {
-          this.timeStopped = true;
-          this.time = this.timeFocus;
+        } else if (this.numberBreakTime > this.numberForLongStop) {
+          this.time = this.timeFocus
+          Time.savePomodoroGlobalTime(this.time);
+          this.numberBreakTime = 0;
+          this.switchTime()
+          this.isBreak = false;
         }
+      }
     },
-    computed: {
-        minutes() {
-            return Math.floor(this.time / 60);
-        },
-        seconds() {
-            return this.time % 60;
-        },
+    switchTime() {
+      this.timeStopped = !this.timeStopped;
     },
-    async mounted() {
-        setInterval(this.updateTime, 1000);
-        this.tasks = await this.getTasks();
-        const { duration, shortbreakDuration, longbreakDuration } = UserService.getPomodoroConfiguration();
-        this.time = Time.minutesToSeconds(duration);
-        this.timeFocus = Time.minutesToSeconds(duration);
-        this.shortBreakTime = Time.minutesToSeconds(shortbreakDuration);
-        this.longBreakTime = Time.minutesToSeconds(longbreakDuration);
+    restart() {
+      this.timeStopped = true;
+      this.time = this.timeFocus;
+    }
+  },
+  computed: {
+    minutes() {
+      return Math.floor(this.time / 60);
     },
-    components: {
-        List,
+    seconds() {
+      return this.time % 60;
     },
+  },
+  async mounted() {
+    // this.time = Time.getPomodoroActualTime();
+    setInterval(this.updateTime, 1000);
+    const {duration, shortbreakDuration, longbreakDuration} = UserService.getPomodoroConfiguration();
+    if (!this.time) {
+      this.time = Time.minutesToSeconds(duration);
+    }
+
+    this.timeFocus = Time.minutesToSeconds(duration);
+    this.shortBreakTime = Time.minutesToSeconds(shortbreakDuration);
+    this.longBreakTime = Time.minutesToSeconds(longbreakDuration);
+  },
+
+  beforeUnmount() {
+    if (!this.timeStopped) {
+      Time.saveUnmountTimestamp();
+    }
+    Time.savePomodoroGlobalTime(this.time);
+  }
 };
 </script>
 
 <style scoped>
 .col-switch {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 </style>
