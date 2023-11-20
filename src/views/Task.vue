3e5267eba35713de1f44d59a375368acb4a5e43b
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2023-11-20 19:55:30 -0300

    recomendation

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -2,8 +2,7 @@
     <v-row class="d-flex justify-end mx-12 mt-12">
         <v-menu open-on-hover>
             <template v-slot:activator="{ props }">
-                <v-btn class="btn-positive-action" v-bind="props">
-                    <svg-icon type="mdi" :path="pathViewIcon"></svg-icon>
+                <v-btn class="btn-positive-action" v-bind="props" prepend-icon="mdi-tune" append-icon="mdi-information" >
                     <p class="mx-4">
                         Visualização
                     </p>
@@ -14,22 +13,24 @@
                 <v-list-item v-for="(item, index) in viewItems" :key="index">
                     <v-list-item-title @click="setViewType(item.viewType)">
                         <v-btn variant="plain">
+                            <v-icon v-if="item.recomendation" color="yellow" icon="mdi-star"></v-icon>
+                            &nbsp;
                             {{ item.title }}
+
                         </v-btn>
                     </v-list-item-title>
                 </v-list-item>
             </v-list>
         </v-menu>
     </v-row>
-    <KanbanBoard v-if="viewType == 'kanban'" />
-    <TaskListView v-if="viewType == 'list'" />
+    <KanbanBoard v-if="viewType === 'kanban'" />
+    <TaskListView v-if="viewType === 'list'" />
 </template>
-  
+
 <script>
 import KanbanBoard from './KanbanBoard.vue';
 import TaskListView from './TaskListView.vue';
 
-import SvgIcon from '@jamescoyle/vue-icon';
 import * as md from '@mdi/js';
 
 export default {
@@ -40,12 +41,14 @@ export default {
                 {
                     title: 'Kanban',
                     icon: 'mdiViewDashboard',
-                    viewType: 'kanban'
+                    viewType: 'kanban',
+                    recomendation: true,
                 },
                 {
                     title: 'Lista',
                     icon: 'mdiViewList',
-                    viewType: 'list'
+                    viewType: 'list',
+                    recomendation: false,
                 }
             ],
             viewType: 'list',
@@ -59,10 +62,9 @@ export default {
     },
     components: {
         KanbanBoard,
-        TaskListView,
-        SvgIcon
+        TaskListView
     }
 }
 </script>
-  
+
 <style scoped></style>
